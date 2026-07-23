25173583fe99210071cd5014e5375e30d0dee968
lukoc1 <lukoc@visa.com>
2026-07-23 16:03:58 +0200

    Dao added from workshop 2 pt2

--- src/main/java/pl/lukasz/entity/User.java
+++ src/main/java/pl/lukasz/entity/User.java
@@ -1,4 +1,4 @@
-package pl.lukasz.web.entity;
+package pl.lukasz.entity;
 
 public class User {
 
--- src/main/java/pl/lukasz/entity/UserDao.java
+++ src/main/java/pl/lukasz/entity/UserDao.java
@@ -1,4 +1,4 @@
-package pl.lukasz.web.entity;
+package pl.lukasz.entity;
 
 import pl.coderslab.utils.DbUtil;
 import pl.coderslab.utils.PasswordUtil;
